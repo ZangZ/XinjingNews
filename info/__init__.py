01f3c2be859a2f94382e41d87019377bba1ec5e0
ZangZ <jinjin1_happy@163.com>
2018-11-03 12:35:31 +0800

    解决redis_store与蓝图注册循环导入的问题&变量类型注释

--- info/__init__.py
+++ info/__init__.py
@@ -5,12 +5,13 @@ from flask_sqlalchemy import SQLAlchemy
 # 可以用来指定 session 保存的位置
 from flask_session import Session
 from config import config
-from info.modules.index import index_blu
+
 
 # 初始化数据库
 #  在Flask很多扩展里面都可以先初始化扩展的对象，然后再去调用 init_app 方法去初始化
 db = SQLAlchemy()
 
+redis_store = None  # type: StrictRedis
 
 def create_app(config_name):
     app = Flask(__name__)
@@ -22,6 +23,7 @@ def create_app(config_name):
 
 
     # 初始化redis 存储对象
+    global redis_store
     redis_store = StrictRedis(host=config[config_name].REDIS_HOST, port=config[config_name].REDIS_PORT)
 
     # 开启当前项目CSRF 保护,制作服务器验证功能
@@ -31,6 +33,7 @@ def create_app(config_name):
     Session(app)
 
     # 注册蓝图
+    from info.modules.index import index_blu
     app.register_blueprint(index_blu)
 
     return app
