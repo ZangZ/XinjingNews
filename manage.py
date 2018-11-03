947631c66317905e2cb56e85f4f20791a3ad980a
ZangZ <jinjin1_happy@163.com>
2018-11-03 11:05:04 +0800

    创建配置类,对数据库/redis/session/csrf进行配置,使用flask_script管理项目

--- manage.py
+++ manage.py
@@ -1,14 +1,63 @@
 
 
+from flask_wtf import CSRFProtect
+from redis import StrictRedis
 
+from flask import Flask, session
+from flask_sqlalchemy import SQLAlchemy
+# 可以用来指定 session 保存的位置
+from flask_session import Session
+from flask_script import Manager
 
+# 1.定义配置类
+class Config(object):
+    """工程配置信息"""
+    Debug = True
+    SECRET_KEY = "iECgbYWReMNxkRprrzMo5KAQYnb2UeZ3bwvReTSt+VSESW0OB8zbglT+6rEcDW9X"
 
-from flask import Flask
+    # 数据库的配置信息
+    SQLALCHEMY_DATABASE_URI = "mysql://root:mysql@127.0.0.1:3306/xinjingnews"
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+
+    # redis配置
+    REDIS_HOST = "127.0.0.1"
+    REDIS_PORT = 6379
+
+    # Session保存配置
+    SESSION_TYPE = "redis"
+    # 开启session签名
+    SESSION_USE_SIGNER = True
+    # 指定 Session 保存的 redis
+    SESSION_REDIS = StrictRedis(host=REDIS_HOST, port=REDIS_PORT)
+    # 设置需要过期
+    SESSION_PERMANENT = False
+    # 设置过期时间
+    PERMANENT_SESSION_LIFETIME = 86400 * 2
+
 
 app = Flask(__name__)
+# 加载配置
+app.config.from_object(Config)
+
+# 初始化数据库
+db = SQLAlchemy(app)
+
+# 初始化redis 存储对象
+redis_store = StrictRedis(host=Config.REDIS_HOST, port=Config.REDIS_PORT)
+
+# 开启当前项目CSRF 保护,制作服务器验证功能
+CSRFProtect(app)
+
+# 设置session保存指定位置
+Session(app)
+
+manager = Manager(app)
+
+
 
 @app.route('/index')
 def index():
+    session["name"] = "laoda"
     return 'index'
 
 if __name__ == '__main__':
