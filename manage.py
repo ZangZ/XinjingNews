1b79c0800484bef63a408cecdfd865d9e8c33135
ZangZ <jinjin1_happy@163.com>
2018-11-03 11:19:43 +0800

    抽取app的创建爱你逻辑到业务逻辑模块里面

--- manage.py
+++ manage.py
@@ -1,34 +1,11 @@
 
-
-from flask_wtf import CSRFProtect
-from redis import StrictRedis
-
-from flask import Flask, session
-from flask_sqlalchemy import SQLAlchemy
-# 可以用来指定 session 保存的位置
-from flask_session import Session
+from flask import session
 from flask_script import Manager
 from flask_migrate import Migrate, MigrateCommand
-from config import Config
 
-app = Flask(__name__)
-# 加载配置
-app.config.from_object(Config)
-
-# 初始化数据库
-db = SQLAlchemy(app)
-
-# 初始化redis 存储对象
-redis_store = StrictRedis(host=Config.REDIS_HOST, port=Config.REDIS_PORT)
-
-# 开启当前项目CSRF 保护,制作服务器验证功能
-CSRFProtect(app)
-
-# 设置session保存指定位置
-Session(app)
+from info import app,db
 
 manager = Manager(app)
-
 # 将 app 与 db 关联
 Migrate(app, db)
 # 将迁移命令添加到manager中
